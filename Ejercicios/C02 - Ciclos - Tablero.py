68e22934d6ec6a58aad36c44ecf75e12f042e878
Carolina <cdimatteo@fi.uba.ar>
2021-04-13 22:34:27 -0300

    Agrego ejercicio de tablero (ciclos)

--- Ejercicios/C02 - Ciclos - Tablero.py
+++ Ejercicios/C02 - Ciclos - Tablero.py
@@ -3,7 +3,7 @@ Crear un programa que permita generar un tablero de nxm en el cual la diagonal s
 Pedir al usuario que ingrese una coordenada (fila y columna).
 Si el casillero tiene un "*" cambiarlo a "#".
 Si  tiene un "#" mostrar un mensaje que diga "la diagonal es del alfil"
-Si elige el mismo casillero más de una vez mostrar un mensaje que diga "¿¿¿Pero que esta haciendo???"
+Si elige el mismo casillero que el turno anterior mostrar un mensaje que diga "¿¿¿Pero que esta haciendo???"
 '''
 
 # Pido dimensiones del tablero
@@ -25,4 +25,32 @@ for i in range(n):
         print(tablero[i][j], end=" ")
     print("] ", end="")
     print()
+
+print("Ingrese una coordenada:")
+x = int(input("Posicion de la fila: "))-1
+y = int(input("Posicion de la columna: "))-1
+old_x = 0
+old_y = 0
+
+while x > 0 and x < n and y > 0 and y < m: # Si es coordenada valida
+    if (old_x != 0 and old_y != 0) and (x == old_x and y == old_y): # Si es coordenada anterior
+        print("¿¿¿Pero que esta haciendo???")
+    else: # Si no es coordenada anterior
+        if tablero[x][y] == "*":
+            tablero[x][y] = "#"
+            # Imprimo el tablero
+            for i in range(n):
+                print("[ ", end="")
+                for j in range(m):
+                    print(tablero[i][j], end=" ")
+                print("] ", end="")
+                print()
+        else:
+            print("la diagonal es del alfil")        
+    # Guardo coordenada actual
+    old_x = x
+    old_y = y
+    print("Ingrese una coordenada:")
+    x = int(input("Posicion de la fila: "))-1
+    y = int(input("Posicion de la columna: "))-1
 
